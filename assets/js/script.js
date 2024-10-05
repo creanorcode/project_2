a0c2cc3369b3428592caea048a391d15d6b6c41c
Thomas Eriksson <p03k9o0w@students.codeinstitute.net>
2024-10-05 22:38:32 +0000

    Updating with a reset-game button, css and html.

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// List with choices
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+const results = {
+    rock: ["scissors", "lizard"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+    lizard: ["spock", "paper"],
+    spock: ["scissors", "rock"],
+};
+
+let currentLevel
+let
+let
+let
